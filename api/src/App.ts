07c35844de13d82009022927bf6e4126537b5f1b
Luis Mario <luismario337y@gmail.com>
2025-09-26 18:51:29 -0300

    feat: created yup schema for validations

--- api/src/App.ts
+++ api/src/App.ts
@@ -1,10 +1,15 @@
 import express, { Express } from 'express';
+import { router } from './routes';
+import "dotenv/config"
+import "./shared/services/translationYUP"
 
 const _APP: Express = express();
 
 const _PORT: string | undefined = process.env.PORT;
 
 _APP.use(express.json());
+
+_APP.use(router);
 
 _APP.listen(_PORT || 2345, () => {
     console.log(`ReForg esta no ar!!! - http://localhost:${_PORT}`);
---
+++ api/src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import { ProductsController } from "./../controllers/index";
+
+export const router: Router = Router();
+
+router.get("/", (_, response) => {
+    response.send("ReForg API");
+});
+
+router.post("/reforg/products", (request, response) => ProductsController.create(request, response));
+
